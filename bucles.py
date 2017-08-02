d28be10abc80600a61d5a685818fbc1a45d02445
Rolando <rolandoaburto7@gmail.com>
2017-08-02 19:42:04 -0400

    Bucles for mas funcion range

--- bucles.py
+++ bucles.py
@@ -1,3 +1,9 @@
 for i in ["Primavera", "Verano", "Otoño", "Invierno"]:
     """Ciclo for"""
     print("Holo "+i)
+
+for i in ["Pildoras","Informaticas","Python"]:
+    print("Hola", end=" ")
+
+for i in range(10):
+    print(i)
