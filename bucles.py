4d73c4bcf3e6b3ad81e6f466da509339e4d43d45
Rolando <rolandoaburto7@gmail.com>
2017-08-03 14:47:03 -0400

    For controlado con principio y fin, mas las funciones f para ocupar variables dentro de strings y reutilizacion de funcion len()

--- bucles.py
+++ bucles.py
@@ -6,4 +6,10 @@ for i in ["Pildoras","Informaticas","Python"]:
     print("Hola", end=" ")
 
 for i in range(10):
+    print(f"Valor de la variable {i}")
+
+for i in range(0,10,2):
+    """For desde hasta con el avance del for"""
     print(i)
+
+print(len("Rolando"))
