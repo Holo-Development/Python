a4bf78ad66cda65b2e96acf6c64d8dfccb4437ea
Rolando <rolandoaburto7@gmail.com>
2017-08-04 18:05:08 -0400

    Bucle While con if para entender la logica, mas el primer import de la clase math, se uso tambien la instruccion break.

--- bucles.py
+++ bucles.py
@@ -1,3 +1,5 @@
+import math
+
 for i in ["Primavera", "Verano", "Otoño", "Invierno"]:
     """Ciclo for"""
     print("Holo "+i)
@@ -13,3 +15,41 @@ for i in range(0,10,2):
     print(i)
 
 print(len("Rolando"))
+
+
+i=1
+
+while i<=10:
+    print(f"Ejecucion {str(i)}")
+    i=i+1
+print("Termino la ejecucion")
+
+
+edad=int(input("Introduce tu edad por favor: "))
+
+while edad<5 or edad>100:
+    print("Has ingresado una edad incorrecta, vuelve a intentarlo")
+    edad=int(input("Introduce tu edad por favor: "))
+
+print("Gracias por colaborar. Puedes pasar")
+print(f"Edad del aspirante es {str(edad)}")
+
+
+
+print("Raiz cuadrada")
+numero=int(input("Introduce un numero positivo por favor: "))
+
+intentos=0
+
+while numero<0:
+    print("No existe una raiz negativa")
+
+    if intentos==2:
+        print("Intentos finalizados")
+        break
+    numero=int(input("Introduce un numero positivo por favor: "))
+    if numero<0:
+        intentos=intentos+1
+if intentos<2:
+    solucion=math.sqrt(numero)
+    print(f"Raiz cuadrada: {solucion} de {numero}")
