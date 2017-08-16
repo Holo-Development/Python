660d5aa620431a5459e31c5291d089c1d5008117
Rolando <rolandoaburto7@gmail.com>
2017-08-16 14:53:08 -0300

    Prueba en SUblimeText

--- POO.py
+++ POO.py
@@ -4,20 +4,26 @@ class Coche():
     ruedas=4
     enmarcha=False
 
-    def arrancar(self):
-        self.enmarcha=True
-        
-    def estado(self):
+    def arrancar(self, arranque):
+        self.enmarcha=arranque
         if self.enmarcha:
             print("El coche esta encendido")
         else:
             print("El coche esta apagado")
+        
+    def estado(self):
+        print(f"Ruedas: {self.ruedas}")
+        print(f"Largo: {self.largoChasis}")
+        print(f"Ancho: {self.anchoChasis}")
 
 
-
+print("------Primer Objeto------")
 miCoche=Coche()
-
 print(f"El coche tiene {miCoche.largoChasis}")
+miCoche.estado()
+print(miCoche.arrancar(True))
 print(miCoche.estado())
-miCoche.arrancar()
-print(miCoche.estado())
+print("------Segundo objeto------")
+segundoCoche=Coche()
+segundoCoche.estado()
+print(segundoCoche.arrancar(False))
