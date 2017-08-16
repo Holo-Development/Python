7fa8a6f306546c1d360e97da4b7d72403a874a35
Rolando <rolandoaburto7@gmail.com>
2017-08-16 17:11:36 -0300

    Se agrego la encapsulacion y el metodo constructor en Python

--- POO.py
+++ POO.py
@@ -1,25 +1,26 @@
 class Coche():
-    largoChasis=250
-    anchoChasis=120
-    ruedas=4
-    enmarcha=False
+    def __init__(self):
+        self.__largoChasis=250
+        self.__anchoChasis=120
+        self.__ruedas=4
+        self.__enmarcha=False
 
     def arrancar(self, arranque):
-        self.enmarcha=arranque
-        if self.enmarcha:
+        self.__enmarcha=arranque
+        if self.__enmarcha:
             print("El coche esta encendido")
         else:
             print("El coche esta apagado")
         
     def estado(self):
-        print(f"Ruedas: {self.ruedas}")
-        print(f"Largo: {self.largoChasis}")
-        print(f"Ancho: {self.anchoChasis}")
+        print(f"Ruedas: {self.__ruedas}")
+        print(f"Largo: {self.__largoChasis}")
+        print(f"Ancho: {self.__anchoChasis}")
 
 
 print("------Primer Objeto------")
 miCoche=Coche()
-print(f"El coche tiene {miCoche.largoChasis}")
+print(f"El coche tiene {miCoche.__largoChasis}")
 miCoche.estado()
 print(miCoche.arrancar(True))
 print(miCoche.estado())
