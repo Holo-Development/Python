3614531de5c04fbb32167844e0b8edc710b67b45
Rolando <rolandoaburto7@gmail.com>
2017-08-17 18:44:53 -0300

    Se agrego encapsulacion a los metodos y se explico mas el concepto de metodos internos que son privados

--- POO.py
+++ POO.py
@@ -7,24 +7,42 @@ class Coche():
 
     def arrancar(self, arranque):
         self.__enmarcha=arranque
+
         if self.__enmarcha:
-            print("El coche esta encendido")
+            chequeo=self.__chequeo_interno()
+
+        if self.__enmarcha and chequeo:
+            return "El coche esta encendido"
+        elif self.__enmarcha and chequeo==False:
+            return "Hay problemas"
         else:
-            print("El coche esta apagado")
+            return "El coche esta apagado"
         
     def estado(self):
         print(f"Ruedas: {self.__ruedas}")
         print(f"Largo: {self.__largoChasis}")
         print(f"Ancho: {self.__anchoChasis}")
 
+    def __chequeo_interno(self): #Metodo encapsulado
+        print("Realizando chequeo interno")
+
+        self.gasolina="Ok"
+        self.aceite="Ok"
+        self.puertas="Cerradas"
+
+        if self.gasolina=="Ok" and self.aceite=="Ok":
+            return True
+        else:
+            return False
+
 
 print("------Primer Objeto------")
 miCoche=Coche()
-print(f"El coche tiene {miCoche.__largoChasis}")
+#print(f"El coche tiene {miCoche.__largoChasis}")
 miCoche.estado()
 print(miCoche.arrancar(True))
 print(miCoche.estado())
 print("------Segundo objeto------")
 segundoCoche=Coche()
 segundoCoche.estado()
-print(segundoCoche.arrancar(False))
+print(segundoCoche.arrancar(True))
